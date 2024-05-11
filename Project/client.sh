1323bf3bbde6fd65ca52292438f3c461af83bea5
!abdullah <muhammadabdullah8040@gmail.com>
2024-05-11 13:57:38 +0500

    Update client.sh

--- Project/client.sh
+++ Project/client.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+# Prompt for host and port
+ascii_art() {
+  echo -e '\033[0;31m'  # Switch to red
+  echo ' ▄▄▄▄    ▄▄▄        ██████  ██░ ██     ▄████▄   ██░ ██  ▄▄▄     ▄▄▄█████▓'
+  echo '▓█████▄ ▒████▄    ▒██    ▒ ▓██░ ██▒   ▒██▀ ▀█  ▓██░ ██▒▒████▄   ▓  ██▒ ▓▒'
+  echo '▒██▒ ▄██▒██  ▀█▄  ░ ▓██▄   ▒██▀▀██░   ▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄ ▒ ▓██░ ▒░'
+  echo '▒██░█▀  ░██▄▄▄▄██   ▒   ██▒░▓█ ░██    ▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██░ ▓██▓ ░ '
+  echo '░▓█   ▀█▓ ▓█   ▓██▒▒██████▒▒░▓█▒░██▓   ▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒ ▒██▒ ░ '
+  echo '░▒▓███▀▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒   ░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░ ▒ ░░   '
+  echo '▒░▒   ░   ▒   ▒▒ ░░ ░▒  ░ ░ ▒ ░▒░ ░     ░  ▒    ▒ ░▒░ ░  ▒   ▒▒ ░   ░    '
+  echo ' ░    ░   ░   ▒   ░  ░  ░   ░  ░░ ░   ░         ░  ░░ ░  ░   ▒    ░      '
+  echo ' ░            ░  ░      ░   ░  ░  ░   ░ ░       ░  ░  ░      ░  ░        '
+  echo '      ░                               ░                             '
+  echo -e '\033[0m'  # Reset color
+}
+
+ascii_art
+
+read -p "Enter the hostname or IP address: " host
+read -p "Enter the port number: " port
+
+communication_file="communication.log"
+# Run netcat command
+nc "$host" "$port"
